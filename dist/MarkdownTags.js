234d5ca6930b734a14f95fb97a0fa6fd1b34517a
DaanV2 <daanverstraten@hotmail.com>
2022-12-26 10:15:16 +0100

    Applying ignore

--- dist/MarkdownTags.js
+++
@@ -1,7 +0,0 @@
-export var MarkdownTags;
-(function (MarkdownTags) {
-    function is(value) {
-        return typeof value === "object" && value !== null && Array.isArray(value.tags);
-    }
-    MarkdownTags.is = is;
-})(MarkdownTags || (MarkdownTags = {}));
--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    transformIgnorePatterns: ["<rootDir>/node_modules/"],
-    testMatch: ["**/test/**/*.test.ts"],
-};
-module.exports = config;
