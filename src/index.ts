18d2ec10f1ecf264eb4332ed6a0988e8a5ab21f4
DaanV2 <daanverstraten@hotmail.com>
2022-12-26 11:31:55 +0100

    Reading from correct ignore input

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as core from "@actions/core";
 async function action() {
   const settings = {
     folder: core.getInput("folder", { trimWhitespace: true }),
-    ignores: core.getMultilineInput("ignores", { trimWhitespace: true }),
+    ignores: core.getMultilineInput("ignore", { trimWhitespace: true }),
   };
 
   console.log("Starting on linking", settings);
